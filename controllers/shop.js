cf72e8a63d0c220f0fdaafacfbd884c61787b227
David Orson <onlyorson@outlook.com>
2020-05-23 15:52:05 +0100

    MySQL setup

--- controllers/shop.js
+++ controllers/shop.js
@@ -23,9 +23,9 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll((products) => {
+  Product.fetchAll().then(([rows, fieldData]) => {
     res.render("shop/index", {
-      prods: products,
+      prods: rows,
       pageTitle: "Shop",
       path: "/",
     });
